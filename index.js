9ca7864a81df69ddd3cce5bf344c6356fff9111e
biomayak <biomayak@yandex.ru>
2024-02-19 17:48:31 +0300

    feat: added headers for axios request

--- index.js
+++ index.js
@@ -33,7 +33,12 @@ async function parseHTML() {
     })
 
     // Request HTML from xbox-now
-    const response = await axios.get('https://www.xbox-now.com/en/deal-list');
+    const response = await axios.get('https://www.xbox-now.com/en/deal-list', {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+            // Add other necessary headers here
+        }
+    });
     const html = response.data;
     let aIsName = true;
 
